7a3aa7d4a474f42e1f9b17cc79ed19cf925e798a
Josh Dales <30500472+joshdales@users.noreply.github.com>
2022-09-22 18:03:35 -0400

    I'm a fool and all the rest methods exist already

--- src/util/rest.ts
+++ src/util/rest.ts
@@ -1,47 +1,11 @@
-import axios from 'axios'
-import { OpenAPIConfig, Client, DeliveryJob } from '@tiny-mile/delivery-sdk'
+import { OpenAPIConfig, Client } from '@tiny-mile/delivery-sdk'
 
 const tinyMileConfig: Partial<OpenAPIConfig> = {
-  BASE: import.meta.env.API_URL + '/api/v1',
-  VERSION: '1',
+  BASE: import.meta.env.API_URL,
   HEADERS: {
     [import.meta.env.API_AUTH_HEADER]: import.meta.env.API_AUTH_TOKEN,
   },
 }
 
 const tinyMileClient = new Client(tinyMileConfig)
-
-axios.create({
-  baseURL: import.meta.env.API_URL + '/api/v1',
-  headers: {
-    [import.meta.env.API_AUTH_HEADER]: import.meta.env.API_AUTH_TOKEN,
-  },
-})
-
-const fetchDeliveryJob = (deliveryJobId: string) =>
-  tinyMileClient.request.request<DeliveryJob>({
-    method: 'GET',
-    url: `/delivery-jobs/${deliveryJobId}`,
-  })
-
-const openRobotLid = (deliveryJobId: string) =>
-  tinyMileClient.request.request({
-    method: 'POST',
-    url: `/delivery-jobs/${deliveryJobId}/couriers/current/open-lid`,
-  })
-
-const orderPickedUp = (deliveryJobId: string) =>
-  tinyMileClient.request.request({
-    method: 'POST',
-    url: `/delivery-jobs/${deliveryJobId}/events/order-picked-up`,
-  })
-
-const orderDroppedOff = (deliveryJobId: string) =>
-  tinyMileClient.request.request({
-    method: 'POST',
-    url: `/delivery-jobs/${deliveryJobId}/events/order-dropped-off`,
-  })
-
 export default tinyMileClient
-
-export { fetchDeliveryJob, openRobotLid, orderDroppedOff, orderPickedUp }
