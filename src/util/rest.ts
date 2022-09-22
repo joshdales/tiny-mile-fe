5e34f4614cda771d41f76d5ee0b3702e28df4fdc
Josh Dales <30500472+joshdales@users.noreply.github.com>
2022-09-22 17:17:01 -0400

    Update rest things to work with the tiny mile client

--- src/util/rest.ts
+++ src/util/rest.ts
@@ -1,71 +1,47 @@
 import axios from 'axios'
+import { OpenAPIConfig, Client, DeliveryJob } from '@tiny-mile/delivery-sdk'
 
-const rest = axios.create({
+const tinyMileConfig: Partial<OpenAPIConfig> = {
+  BASE: import.meta.env.API_URL + '/api/v1',
+  VERSION: '1',
+  HEADERS: {
+    [import.meta.env.API_AUTH_HEADER]: import.meta.env.API_AUTH_TOKEN,
+  },
+}
+
+const tinyMileClient = new Client(tinyMileConfig)
+
+axios.create({
   baseURL: import.meta.env.API_URL + '/api/v1',
   headers: {
     [import.meta.env.API_AUTH_HEADER]: import.meta.env.API_AUTH_TOKEN,
   },
 })
 
-export interface DeliveryJobResponse {
-  uuid: string
-  stage: 'courier-assignment' | 'pick-up' | 'drop-off' | 'delivery-completed' | 'delivery-canceled'
-  deliveryOrderUUID: string
-  pickUpAddress: {
-    geocodedAddress: {
-      lat: number
-      lng: number
-      googlePlaceID: string
-    }
-    addressDescription: {
-      addressLine1: string
-      addressLine2: string
-      locality: string
-      state: string
-      country: string
-      postalCode: string
-      establishmentName: string
-    }
-  }
-  pickUpEstimatedAt: string
-  dropOffAddress: {
-    geocodedAddress: {
-      lat: number
-      lng: number
-      googlePlaceID: string
-    }
-    addressDescription: {
-      addressLine1: string
-      addressLine2: string
-      locality: string
-      state: string
-      country: string
-      postalCode: string
-      establishmentName: string
-    }
-  }
-  dropOffEstimatedAt: string
-  currentCourierUUID: string
-  nextCourierUUID: string
-  completedAt: string
-  deliveryCharges: {
-    totalInCents: number
-  }
-  packageHolder: 'shipper' | 'receiver' | 'courier' | 'unknown'
-}
-
 const fetchDeliveryJob = (deliveryJobId: string) =>
-  rest.get<DeliveryJobResponse>(`/delivery-jobs/${deliveryJobId}`)
+  tinyMileClient.request.request<DeliveryJob>({
+    method: 'GET',
+    url: `/delivery-jobs/${deliveryJobId}`,
+  })
 
-const openRobotLid = (deliveryJobId: string, requestData: { reason: string; message: string }) =>
-  rest.post(`/delivery-jobs/${deliveryJobId}/couriers/current/open-lid`, requestData)
+const openRobotLid = (deliveryJobId: string) =>
+  tinyMileClient.request.request({
+    method: 'POST',
+    url: `/delivery-jobs/${deliveryJobId}/couriers/current/open-lid`,
+  })
 
 const orderPickedUp = (deliveryJobId: string) =>
-  rest.post(`/delivery-jobs/${deliveryJobId}/events/order-picked-up`)
+  tinyMileClient.request.request({
+    method: 'POST',
+    url: `/delivery-jobs/${deliveryJobId}/events/order-picked-up`,
+  })
 
 const orderDroppedOff = (deliveryJobId: string) =>
-  rest.post(`/delivery-jobs/${deliveryJobId}/events/order-dropped-off`)
+  tinyMileClient.request.request({
+    method: 'POST',
+    url: `/delivery-jobs/${deliveryJobId}/events/order-dropped-off`,
+  })
 
-export default rest
+export default tinyMileClient
 
 export { fetchDeliveryJob, openRobotLid, orderDroppedOff, orderPickedUp }
