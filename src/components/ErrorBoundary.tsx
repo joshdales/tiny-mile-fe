481c731d429216a80c3ea5a63eee3968e532f0ea
Josh Dales <30500472+joshdales@users.noreply.github.com>
2022-09-22 20:34:35 -0400

    Update the ErrorBoundary

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import styles from './ErrorBoundary.module.css'
+
+import ErrorMessage from './ErrorMessage'
 
 interface iState {
   error: boolean
@@ -19,12 +20,9 @@ export default class ErrorBoundary extends React.PureComponent<any, iState> {
   render(): JSX.Element {
     if (this.state.error) {
       return (
-        <div className={styles.container}>
-          <p className={styles.message}>
-            Oh no! Something has gone wrong <span>💀</span>
-          </p>
-          <p className={styles.message}>{this.state.errorMessage}</p>
-        </div>
+        <ErrorMessage errorMessage={this.state.errorMessage || ''}>
+          Oh no! Something has gone very wrong <span>💀</span>
+        </ErrorMessage>
       )
     } else {
       return this.props.children
